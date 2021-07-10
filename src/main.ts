55e1ea66a3ba9fd72865c955f4c8e86803f0c7c7
victordelajarte <29466815+victordelajarte@users.noreply.github.com>
2021-07-11 00:37:57 +0200

    ✨ Add global interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { ErrorHandlingInterceptor } from './interceptors/error-handling.interceptor';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new ErrorHandlingInterceptor(),
+  );
   await app.listen(3000);
 }
 bootstrap();

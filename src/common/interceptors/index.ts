7890404235660c502eb8367f6b923f3d5901e04c
victordelajarte <29466815+victordelajarte@users.noreply.github.com>
2021-07-11 01:46:22 +0200

    👍 Improve interceptors export

---
+++ src/common/interceptors/index.ts
@@ -0,0 +1,3 @@
+export * from './authentication.interceptor';
+export * from './error-handling.interceptor';
+export * from './logging.interceptor';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
-import { ErrorHandlingInterceptor } from './common/interceptors/error-handling.interceptor';
+import {
+  LoggingInterceptor,
+  ErrorHandlingInterceptor,
+} from './common/interceptors';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap(): Promise<void> {

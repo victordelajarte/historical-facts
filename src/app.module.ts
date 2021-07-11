feca9a53f642ebccd64858e5f3a3c278f36e2908
victordelajarte <29466815+victordelajarte@users.noreply.github.com>
2021-07-11 02:28:44 +0200

    🔥 Remove useless files & tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { AuthenticationInterceptor } from './common/interceptors/authentication.interceptor';
 import { FactsModule } from './facts/facts.module';
@@ -17,13 +15,11 @@ import { FactsModule } from './facts/facts.module';
     AuthenticationModule,
     FactsModule,
   ],
-  controllers: [AppController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: AuthenticationInterceptor,
     },
-    AppService,
   ],
 })
 export class AppModule {}
